3f7dcde70b2a6c71303379600025328b0354bb7c
naresh <mail2nareshfd@gmail.com>
2022-10-04 13:46:26 +0530

    first commit

---
+++ src/components/Element.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Checkbox from './FormElements/Checkbox';
+import Input from './FormElements/Input';
+import Select from './FormElements/Select';
+const Element = ({ field: { field_type, field_id, field_label, field_placeholder, field_value, field_options } }) => {
+
+    switch (field_type) {
+        case 'text':
+            return (<Input
+                field_id={field_id}
+                field_label={field_label}
+                field_placeholder={field_placeholder}
+                field_value={field_value}
+
+            />)
+        case 'select':
+            return (<Select
+                field_id={field_id}
+                field_label={field_label}
+                field_placeholder={field_placeholder}
+                field_value={field_value}
+                field_options={field_options}
+            />)
+        case 'checkbox':
+            return (<Checkbox
+                field_id={field_id}
+                field_label={field_label}
+                field_value={field_value}
+            />)
+
+        default:
+            return null;
+    }
+
+
+}
+
+export default Element
